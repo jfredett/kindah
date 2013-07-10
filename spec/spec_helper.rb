728b9ddbf32178dd1ed0a5fdcc3b9488bae8e5ae
Joe Fredette <jfredett@gmail.com>
2013-07-10 09:23:03 -0400

    add spec/spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'crystalline/spec'
+
+#include helpers
+Dir["./spec/helpers/*.rb"].each { |file| require file }
+
+#include shared examples
+Dir["./spec/shared/*_examples.rb"].each { |file| require file }
+
+Coveralls.wear! if ENV['COVERALLS']
+
+Crystalline::Spec.install!
+
